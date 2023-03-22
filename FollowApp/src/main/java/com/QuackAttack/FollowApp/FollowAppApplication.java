78efe328f73732481487b542503def58df554ac2
Yun <81588081+YunFeng-Zheng@users.noreply.github.com>
2023-03-22 21:55:21 +0100

    Add files via upload

---
+++ FollowApp/src/main/java/com/QuackAttack/FollowApp/FollowAppApplication.java
@@ -0,0 +1,13 @@
+package com.QuackAttack.FollowApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FollowAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FollowAppApplication.class, args);
+	}
+
+}
---
+++ FollowApp/src/test/java/com/QuackAttack/FollowApp/FollowAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.QuackAttack.FollowApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FollowAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
